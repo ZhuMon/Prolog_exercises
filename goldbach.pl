ff90d396c4d9c4ee01d8fb170e5d336918062bc1
YuXiang <richardlin0212@gmail.com>
2019-05-21 12:11:19 +0800

    Finish goldbach.pl

--- goldbach.pl
+++ goldbach.pl
@@ -17,8 +17,14 @@ is_prime(A) :-
     \+ divisible(A,X),
     is_prime_recursion(A, X).
 
-gold_bach(A, N) :-
-    X is (A/2) + N,
-    Y is (A/2) - N,
-    is_prime(X),
-    is_prime(Y).
+gold_bach_recursion(A, N, X, Y) :-
+    N < A-1,
+    N1 is N + 1,
+    X1 is (A/2) - N1,
+    Y1 is (A/2) + N1,
+    ((is_prime(X1), is_prime(Y1), X is X1, Y is Y1);(gold_bach_recursion(A,N1,X,Y))).
+    
+gold_bach(A) :-
+    N is 0,
+    gold_bach_recursion(A, N, X, Y),
+    write(X),write(" "),write(Y).
